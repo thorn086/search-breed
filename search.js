14b198aaf6c288639a97e10a3f35dc882848ef2b
Darren Rose <50759967+thorn086@users.noreply.github.com>
2019-07-17 13:04:53 -0500

    breed search

---
+++ search.js
@@ -0,0 +1,49 @@
+'use strict';
+
+
+function submitForm() {
+    $('form').submit(event => {
+        event.preventDefault();
+        let searchChoice = document.getElementById("search").value;
+        let breed = searchChoice.toLowerCase();
+        loadImages(breed);
+        resetForm();
+    });
+}
+
+
+function displayImages(dogImageJson){
+    $('.results-img').empty();
+    console.log(dogImageJson);
+    let x ="";
+    for (let i = 0; i < dogImageJson.message.length; i++) {
+        x += `<img src="${dogImageJson.message[i]}" alt=" Dog Breed Image">`;
+    };
+    $('.results-img').append(x);
+}
+
+function error(dogImageJson){
+    console.log(dogImageJson);
+    $('.results-img').empty();
+    $('.results-img').append(`<div>${dogImageJson.message}</div>`);
+}
+
+function loadImages(breed) {
+    fetch("https://dog.ceo/api/breed/"+`${breed}`+"/images")
+        .then(dogImage => dogImage.json())
+        .then(dogImageJson => displayImages(dogImageJson))
+        .catch(dogImageJson => error(dogImageJson));
+}
+
+
+
+function resetForm(){
+    $('form').trigger('reset');
+}
+
+
+$(function () {
+    console.log('app loaded, ready for submition');
+    loadImages("hound");
+    submitForm();
+});
